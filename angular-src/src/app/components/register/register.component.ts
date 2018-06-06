1cbc1b284aa8c62b543e3fb323e9454a18878792
manitty <pminniti94@gmail.com>
2018-06-05 21:36:27 -0400

    060518

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+name: String;
+username: String;
+email: String;
+password;
+
+  constructor(private validateService: ValidateService, private FlashMessages:FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit(){
+    const user = {  
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    //Required Fields
+    if(!this.validateService.validateRegister(user)){
+      this.FlashMessages.show('Please fill in all Fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    //Validate Email 
+    if(!this.validateService.validateEmail(user.email)){
+      this.FlashMessages.show('Please use a valid Email', {cssClass: 'alert-danger', timeout: 3000});    
+      return false;
+    }
+
+  }
 }
